c35e62df341912b4f21fb86e70c71fa4c01ebd7c
sunn02 <sollopez0002@hotmail.com>
2025-02-17 20:35:15 -0300

    Update Android settings.gradle to version 8.2.1, add image picker functionality to home screen and pick image screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import '/screens/pick_image_screen.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +20,17 @@ class MyHomePage extends StatelessWidget {
                   print('Boton 1');
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const PickImagePage()),
+                    MaterialPageRoute(builder: (context) => const PickImagePage(source: ImageSource.camera,)),
                   );
                 },
                 child: const Text('📷 Tomar Foto')),
             ElevatedButton(
                 onPressed: () {
                   print('Boton 2');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const PickImagePage(source: ImageSource.gallery)),
+                  );
                 },
                 child: const Text('🖼️ Elegir de la Galería ')),
           ],
--- lib/screens/pick_image_screen.dart
+++ lib/screens/pick_image_screen.dart
@@ -1,24 +1,47 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-
-
+import 'package:image_picker/image_picker.dart';
 
 class PickImagePage extends StatefulWidget {
-  const PickImagePage({super.key});
+  final ImageSource source; // Recibe si es cámara o galería
+
+  const PickImagePage({super.key, required this.source});
 
   @override
   State<PickImagePage> createState() => _PickImagePageState();
 }
 
 class _PickImagePageState extends State<PickImagePage> {
+  File? _image;
+  final _picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    pickImage(); // Llama a la función para abrir cámara/galería automáticamente
+  }
+
+
+  pickImage() async {
+    final pickedFile = await _picker.pickImage(source: widget.source);
+    if(pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+        });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        
-      ), 
+      body: Center(child: _image == null
+            ? const Text('No se ha seleccionado ninguna imagen')
+            : Image.file(_image!), // Muestra la imagen seleccionada
+            )
     );
   }
 }
